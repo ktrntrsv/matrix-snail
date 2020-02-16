78f8c8125d16ee7e1516df3eab5e46d90d5da063
ktrntrsv <40740806+ktrntrsv@users.noreply.github.com>
2020-02-16 23:07:28 +0300

    Create martix.py

---
+++ martix.py
@@ -0,0 +1,12 @@
+x,i,j=1,0,0
+n=int(input())
+a=[[0]*n for i in range (n)]
+a[n//2][n//2]=n**2
+for s in range (n//2):
+    for rotate in range (4):
+        for j in range (n-1-s*2):
+            a[i+s][j+s]=x
+            x=x+1
+        a=[[a[i][j] for i in range(n)] for j in range (n-1,-1,-1)]
+for i in a:
+    print(*i)
